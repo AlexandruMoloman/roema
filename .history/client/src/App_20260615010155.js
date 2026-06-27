e65ed429a480dc0fa167fa54765e6e788d85bed1
Alexandru Moloman <dekabriskii94@gmail.com>
2026-06-27 21:59:43 +0200

    roema full commit

---
+++ .history/client/src/App_20260615010155.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import './App.css';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
